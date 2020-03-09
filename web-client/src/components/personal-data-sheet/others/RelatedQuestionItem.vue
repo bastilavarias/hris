bcfe3ea48207eea35250cdec5fb09720e0a303e5
Sebastian Curtis T. Lavarias <denzilbao@gmail.com>
2020-03-09 15:40:58 +0800

    added personal data sheet / related questions.

---
+++ web-client/src/components/personal-data-sheet/others/RelatedQuestionItem.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p class="black--text body-2 d-block">{{sampleQuestion}}</p>
+		<v-radio-group row>
+			<v-radio label="Yes" value="yes"></v-radio>
+			<v-radio label="No" value="no"></v-radio>
+		</v-radio-group>
+		<v-text-field label="Details"></v-text-field>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "related-question-item",
+
+        data() {
+            return {
+                sampleQuestion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est iusto nobis quis quos unde voluptatibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est iusto nobis quis quos unde voluptatibus!"
+            };
+        }
+    };
+</script>
--- web-client/src/components/personal-data-sheet/others/RelatedQuestions.vue
+++ web-client/src/components/personal-data-sheet/others/RelatedQuestions.vue
@@ -1,11 +1,17 @@
 <template>
 	<v-card>
 		<v-card-title>Related Questions</v-card-title>
+		<v-card-text>
+			<related-question-item></related-question-item>
+			<related-question-item></related-question-item>
+		</v-card-text>
 	</v-card>
 </template>
 
 <script>
-	export default {
-	    name: "personal-data-sheet-related-questions"
-	}
+	import RelatedQuestionItem from "./RelatedQuestionItem";
+    export default {
+	    name: "personal-data-sheet-related-questions",
+        components: {RelatedQuestionItem}
+    }
 </script>
