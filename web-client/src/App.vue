fdd07e9a2eb11fc25e4fae25749efbe5c70736a1
Sebastian Curtis T. Lavarias <denzilbao@gmail.com>
2020-05-16 11:08:49 +0800

    login page design created.

---
+++ web-client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Roboto Condensed", sans-serif;
+  background-color: #e9ebee;
+}
+</style>
---
+++ web-client/src/pages/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height">
+        <v-row justify="center">
+          <v-col cols="12" sm="8" md="6">
+            <div class="mb-10 text-center">
+              <v-avatar :size="100" class="text-center mb-5">
+                <v-img
+                  src="../assets/logo.png"
+                  lazy-src="../assets/logo.png"
+                ></v-img>
+              </v-avatar>
+              <h2>Human Resources Information System</h2>
+            </div>
+            <span class="overline d-block mb-2">Login your credentials.</span>
+            <v-text-field outlined label="Username"></v-text-field>
+            <v-text-field
+              outlined
+              label="Username"
+              type="password"
+            ></v-text-field>
+            <v-btn color="primary" block>Login</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
