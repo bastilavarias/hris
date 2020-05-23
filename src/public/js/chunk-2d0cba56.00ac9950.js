498d83cf18c5c7aa5246e86e4fe96c992b80a1b6
Sebastian Curtis T. Lavarias <denzilbao@gmail.com>
2020-05-23 08:22:28 +0800

    build files and added now configs.

---
+++ src/public/js/chunk-2d0cba56.00ac9950.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cba56"],{"4b0b":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},c=[],r=t("2877"),l={},a=Object(r["a"])(l,u,c,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0cba56.00ac9950.js.map
--- web-client/dist/js/chunk-2d0cba56.2d959ff5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cba56"],{"4b0b":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},c=[],r=t("2877"),l={},a=Object(r["a"])(l,u,c,!1,null,null,null);n["default"]=a.exports}}]);
-//# sourceMappingURL=chunk-2d0cba56.2d959ff5.js.map
