78da227b64b1c9224357bf30029280b94aa81b2d
Sebastian Curtis T. Lavarias <denzilbao@gmail.com>
2020-05-23 09:12:47 +0800

    removed pg dependencies

--- knexfile.js
+++ knexfile.js
@@ -20,18 +20,4 @@ module.exports = {
       directory: __dirname + "/src/db/seeds",
     },
   },
-
-  production: {
-    client: "pg",
-
-    connection: process.env.DATABASE_URL,
-
-    migrations: {
-      directory: __dirname + "/src/db/migrations",
-    },
-
-    seeds: {
-      directory: __dirname + "/src/db/seeds",
-    },
-  },
 };
